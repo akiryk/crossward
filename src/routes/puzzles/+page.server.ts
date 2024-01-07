bc6b49af295ce3e11348deb7ca7cb3a81f8ae32b
adam kiryk <akiryk@wayfair.com>
2024-01-07 13:13:28 -0500

    handle error in a poor way

--- src/routes/puzzles/+page.server.ts
+++ src/routes/puzzles/+page.server.ts
@@ -10,17 +10,21 @@ type Props = {
 export const load: PageServerLoad = async (): Promise<Props> => {
 	// MongoDB returns the _id field by default, which is unserializable.
 	// I could remove it with a projection, _id: 0, but we need it.
-	const unserializablePuzzles = await puzzlesCollection
-		.find({}, { limit: 10, projection: { title: 1 } })
-		.toArray();
+	try {
+		const unserializablePuzzles = await puzzlesCollection
+			.find({}, { limit: 10, projection: { title: 1 } })
+			.toArray();
 
-	// make the _id field serializable
-	const puzzles = unserializablePuzzles.map((puzzle: Puzzle) => ({
-		...puzzle,
-		_id: puzzle._id.toString()
-	}));
+		// make the _id field serializable
+		const puzzles = unserializablePuzzles.map((puzzle: Puzzle) => ({
+			...puzzle,
+			_id: puzzle._id.toString()
+		}));
 
-	return {
-		puzzles
-	};
+		return {
+			puzzles
+		};
+	} catch (error) {
+		return { puzzles: [] };
+	}
 };
