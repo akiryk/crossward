c69215162833d69ec8bc9179d22b549ae150e930
adam kiryk <akiryk@wayfair.com>
2024-01-07 22:13:05 -0500

    undo layout changes

--- src/routes/puzzles/+page.svelte
+++ src/routes/puzzles/+page.svelte
@@ -9,16 +9,21 @@
 
 <section>
 	<h1 class="mb-2 mt-0 text-2xl font-medium leading-tight text-primary">Puzzles</h1>
-	<ul class="w-96">
-		{#each puzzles as puzzle}
-			<li
-				class="w-full border-b-2 border-neutral-100 border-opacity-100 py-4 dark:border-opacity-50"
-			>
-				<span
-					>{puzzle.title}
-					<span><a href="/puzzles/{puzzle._id}" class="text-sky-600">edit</a></span>
-				</span>
-			</li>
-		{/each}
-	</ul>
+	{#if session?.user}
+		<ul class="w-96">
+			{#each puzzles as puzzle}
+				<li
+					class="w-full border-b-2 border-neutral-100 border-opacity-100 py-4 dark:border-opacity-50"
+				>
+					<span
+						>{puzzle.title}
+						<span><a href="/puzzles/{puzzle._id}" class="text-sky-600">edit</a></span>
+					</span>
+				</li>
+			{/each}
+		</ul>
+	{:else}
+		<p>Look buddy,you need to log in before you access this special section</p>
+		<p>Hint: go <a href="/">home</a> and try again</p>
+	{/if}
 </section>
