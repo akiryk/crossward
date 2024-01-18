0f1796b76b22e36500c31a8007d3629669294de4
adam kiryk <akiryk@wayfair.com>
2024-01-18 08:48:34 -0500

    works with ssr now

--- src/routes/polls/+page.svelte
+++ src/routes/polls/+page.svelte
@@ -6,25 +6,21 @@
 	export let storeData: Puzzle | null;
 
 	export let data;
-	// destructure puzzles from data
-	$: ({ puzzle } = data);
+
+	$: ({ puzzle: ssrData } = data);
 
 	const unsubscribe = PuzzleStore.subscribe((data) => {
 		storeData = data;
 	});
 
 	onMount(() => {
-		PuzzleStore.set(puzzle);
-		console.log('storeData', storeData);
+		PuzzleStore.set(data.puzzle);
+		console.log('storeData');
 	});
 
 	onDestroy(() => {
 		unsubscribe();
 	});
-
-	let puzzleData: Puzzle | null = puzzle;
-
-	$: puzzleData = storeData;
 
 	export let handleChange1 = (e) => {
 		console.log(e.target.value);
@@ -37,35 +33,37 @@
 		storeData.grid.cellMap['0:1'].correctValue = e.target.value;
 		storeData.set({ ...storeData });
 	};
-
-	$: puzzleData;
 </script>
 
 <div>
-	{#if puzzleData}
-		<h1 class="text-lg font-bold">{puzzleData.title}</h1>
-		<p>The value is: {puzzleData.grid.cellMap['0:0'].correctValue}</p>
-		<p class="mb-4">The value is: {puzzleData.grid.cellMap['0:1'].correctValue}</p>
+	<h1 class="text-lg font-bold">{storeData?.title || ssrData.title}</h1>
+	<p>
+		The value is: {storeData?.grid.cellMap['0:0'].correctValue ||
+			ssrData.grid.cellMap['0:0'].correctValue}
+	</p>
+	<p class="mb-4">
+		The value is: {storeData?.grid.cellMap['0:1'].correctValue ||
+			ssrData.grid.cellMap['0:1'].correctValue}
+	</p>
 
+	<div class="mb-4">
+		<h1 class=" font-medium">Cell 0:0</h1>
 		<div class="mb-4">
-			<h1 class=" font-medium">Cell 0:0</h1>
-			<div class="mb-4">
-				<input
-					class="border border-solid border-gray-300 mb-4"
-					type="text"
-					on:input={handleChange1}
-				/>
-			</div>
+			<input
+				class="border border-solid border-gray-300 mb-4"
+				type="text"
+				on:input={handleChange1}
+			/>
 		</div>
+	</div>
+	<div class="mb-4">
+		<h1 class=" font-medium">Cell 0:1</h1>
 		<div class="mb-4">
-			<h1 class=" font-medium">Cell 0:1</h1>
-			<div class="mb-4">
-				<input
-					class="border border-solid border-gray-300 mb-4"
-					type="text"
-					on:input={handleChange2}
-				/>
-			</div>
+			<input
+				class="border border-solid border-gray-300 mb-4"
+				type="text"
+				on:input={handleChange2}
+			/>
 		</div>
-	{/if}
+	</div>
 </div>
