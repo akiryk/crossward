e5e4e6bf6a6cb88b4676034d1e65dcc5597995f3
adam kiryk <akiryk@wayfair.com>
2024-01-22 22:30:51 -0500

    save and refresh page

--- src/routes/puzzles/[id]/+page.svelte
+++ src/routes/puzzles/[id]/+page.svelte
@@ -40,7 +40,22 @@
 
 		<h2 class="font-medium text-xl mb-3">{puzzle.title}</h2>
 		{#if dynamicPuzzle || puzzle}
-			<form method="POST" action={'?/updateCellMap'}>
+			<form
+				method="POST"
+				action={'?/updateCellMap'}
+				use:enhance={({ formElement, formData, action, cancel, submitter }) => {
+					// `formElement` is this `<form>` element
+					// `formData` is its `FormData` object that's about to be submitted
+					// `action` is the URL to which the form is posted
+					// calling `cancel()` will prevent the submission
+					// `submitter` is the `HTMLElement` that caused the form to be submitted
+					console.log(formData);
+					return async ({ result, update }) => {
+						// `result` is an `ActionResult` object
+						// `update` is a function which triggers the default logic that would be triggered if this callback wasn't set
+					};
+				}}
+			>
 				<input type="hidden" name="cellMap" value={JSON.stringify(dynamicPuzzle?.cellMap)} />
 				<input type="hidden" name="id" value={puzzle._id} />
 				<div class="mb-5">
