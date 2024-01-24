c014e4722e805ca15ebb802266890069043b0e97
Adam Kiryk <adamkiryk@macbook-pro.mynetworksettings.com>
2024-01-24 06:51:37 -0500

    space buttons

--- src/routes/puzzles/[id]/editPuzzle/+page.svelte
+++ src/routes/puzzles/[id]/editPuzzle/+page.svelte
@@ -64,10 +64,12 @@
 					<Crossword puzzle={dynamicPuzzle || puzzle} gameStatus={GameStatus.EDIT_GRID} />
 				</div>
 				<div class="mb-5 flex">
-					<Button buttonType="submit">Save for later</Button>
+					<div class="mr-5">
+						<Button buttonType="submit">Save for later</Button>
+					</div>
 					<button
 						formaction="?/createHints"
-						class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 "
+						class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5"
 						>Make Hints</button
 					>
 				</div>
