21f932431b3288aaa677f8fa263f8fd449b07ccb
Adam Kiryk <adamkiryk@adams-mbp.mynetworksettings.com>
2024-01-24 15:47:24 -0500

    hints are all saved and reshown

--- src/routes/puzzles/[id]/editHints/+page.server.ts
+++ src/routes/puzzles/[id]/editHints/+page.server.ts
@@ -5,43 +5,67 @@ import { puzzlesCollection } from '$db/puzzles';
 import { handleSanitizeInput } from '$utils/helpers';
 import type { RequestEvent } from './$types';
 import { pageServerLoad } from '../serverHelpers';
+import { GRID_SIZES, EDIT_PUZZLE, PUBLISHED, EDIT_HINTS } from '$utils/constants';
 
 export const load = pageServerLoad;
 
+async function getFilterAndUpdateDocument(
+	request: Request,
+	publishStatus: string
+): Promise<{ id: string; filter: mongodb.Filter<mongodb.BSON.Document>; updateDocument: any }> {
+	const data = await request.formData();
+	const acrossHints = await data.get('acrossHints');
+	const downHints = await data.get('downHints');
+	const id = data.get('id');
+	if (
+		!id ||
+		typeof id !== 'string' ||
+		!acrossHints ||
+		typeof acrossHints !== 'string' ||
+		!downHints ||
+		typeof downHints !== 'string'
+	) {
+		// log error
+		throw new Error('no hinting data');
+	}
+
+	const filter = {
+		_id: new ObjectId(id)
+	};
+	const updateDocument = {
+		$set: {
+			acrossHints: JSON.parse(acrossHints),
+			downHints: JSON.parse(downHints),
+			publishStatus
+		}
+	};
+
+	return { filter, updateDocument, id };
+}
+
 export const actions = {
 	saveHints: async ({ request }: RequestEvent) => {
-		const data = await request.formData();
-		const acrossHints = await data.get('acrossHints');
-		const downHints = await data.get('downHints');
-		const id = data.get('id');
-		if (
-			!id ||
-			typeof id !== 'string' ||
-			!acrossHints ||
-			typeof acrossHints !== 'string' ||
-			!downHints ||
-			typeof downHints !== 'string'
-		) {
-			// log error
-			return;
-		}
-
-		const filter = {
-			_id: new ObjectId(id)
-		};
-		const updateDocument = {
-			$set: {
-				acrossHints: JSON.parse(acrossHints),
-				downHints: JSON.parse(downHints)
-			}
-		};
-
 		try {
+			const { filter, updateDocument, id } = await getFilterAndUpdateDocument(request, EDIT_HINTS);
 			await puzzlesCollection.updateOne(filter, updateDocument);
 			return {
 				status: 303, // HTTP status for "See Other"
 				headers: {
 					location: `/puzzles/${id}/editHints`
+				}
+			};
+		} catch {
+			//
+		}
+	},
+	publish: async ({ request }: RequestEvent) => {
+		try {
+			const { filter, updateDocument, id } = await getFilterAndUpdateDocument(request, PUBLISHED);
+			await puzzlesCollection.updateOne(filter, updateDocument);
+			return {
+				status: 303, // HTTP status for "See Other"
+				headers: {
+					location: `/puzzles/${id}/play`
 				}
 			};
 		} catch {
