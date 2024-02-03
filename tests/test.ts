d7f4f6d187e72bb77df7bee7f78d8c19b4afabfe
Adam Kiryk <adamkiryk@adams-mbp.mynetworksettings.com>
2024-02-03 07:13:59 -0500

    reset script

--- tests/test.ts
+++ tests/test.ts
@@ -4,3 +4,11 @@ test('index page has expected h1', async ({ page }) => {
 	await page.goto('/');
 	await expect(page.getByRole('heading', { name: 'Crossward man' })).toBeVisible();
 });
+
+test('It has navigation to puzzles page', async ({ page }) => {
+	await page.goto('/');
+
+	// Find nav and click it
+	page.getByRole('link', { name: 'Puzzles' }).click();
+	await expect(page.getByRole('heading', { name: 'Login page' })).toBeVisible();
+});
