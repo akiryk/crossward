b0a8c7ce3cc42d92cee3a58e08fd66c397aa063e
Adam Kiryk <adamkiryk@adams-mbp.mynetworksettings.com>
2024-02-04 16:53:51 -0500

    toggle button

--- src/routes/puzzles/[id]/editGrid/+page.svelte
+++ src/routes/puzzles/[id]/editGrid/+page.svelte
@@ -67,13 +67,7 @@
 		unsubscribePuzzleStore();
 	});
 
-	const handleOnPreview = () => {
-		isPreview = true;
-	};
-
-	const handleOffPreview = () => {
-		isPreview = false;
-	};
+	const handleTogglePreview = () => {};
 
 	async function saveData(data: FormData) {
 		// don't try to save if we redirect to hints page because data is already saved
@@ -205,15 +199,10 @@
 						class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5"
 						>Make Hints</button
 					>
-					<button
-						type="button"
-						on:mousedown={handleOnPreview}
-						on:mouseup={handleOffPreview}
-						on:mouseout={handleOffPreview}
-						on:blur={handleOffPreview}
-						class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5"
-						>Preview</button
-					>
+					<label for="togglePreview">
+						<input id="togglePreview" type="checkbox" on:change={handleTogglePreview} />
+						Toggle Preview Mode -
+					</label>
 				</div>
 			</form>
 		{/if}
