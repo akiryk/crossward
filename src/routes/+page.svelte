3e77279adfaa95c354092b8d012abb86caf7c778
Adam Kiryk <adamkiryk@adams-mbp.mynetworksettings.com>
2024-02-05 17:06:15 -0500

    fix layout on home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Crossward man</h1>
-<p>This is all about the crossward</p>
+<div class="p-4">
+	<div class="w-fit">
+		<h1>Crossward man</h1>
+		<p>This is all about the crossward</p>
+	</div>
+</div>
