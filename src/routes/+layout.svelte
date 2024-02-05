58142d5c548a02443a45d3d8bc5c00674fb0976d
Adam Kiryk <adamkiryk@adams-mbp.mynetworksettings.com>
2024-02-05 16:29:15 -0500

    color new heading

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,6 @@
 	import { signIn, signOut } from '@auth/sveltekit/client';
 	import { page } from '$app/stores';
 	import '../app.css';
-
-	export let classes =
-		'text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5';
 </script>
 
 <div class="p-4 max-w-screen-xl">
