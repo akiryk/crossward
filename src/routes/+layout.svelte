5a07389621e7e7123d49e396a6b39526d53a1e41
Adam Kiryk <adamkiryk@adams-mbp.mynetworksettings.com>
2024-02-07 07:21:53 -0500

    fixin

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,7 +7,7 @@
 <div class="p-4 max-w-screen-xl">
 	<nav class="bg-white border-gray-200">
 		<div class="flex flex-wrap items-center justify-between mx-auto py-4">
-			<a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
+			<a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
 				<span class="self-center text-2xl font-semibold whitespace-nowrap">Crossward</span>
 			</a>
 			<button
@@ -38,28 +38,6 @@
 				<ul
 					class="font-medium flex flex-col items-center p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white"
 				>
-					<li>
-						<a
-							href="/"
-							class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0"
-							aria-current="page">Home</a
-						>
-					</li>
-
-					<li>
-						<a
-							href="/puzzles"
-							class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
-							>Puzzles</a
-						>
-					</li>
-					<li>
-						<a
-							href="/about"
-							class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
-							>About</a
-						>
-					</li>
 					<li>
 						{#if $page.data.session?.user}
 							<button
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,97 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+
+	import type { PageData, ActionData } from './$types';
+	import type { Puzzles } from '$utils/types';
+	import { EDIT_PUZZLE, PUBLISHED, MINI, DAILY, SUNDAY, EDITING_HINTS } from '$utils/constants';
+
+	import List from '$components/List.svelte';
+	import ListItem from '$components/ListItem.svelte';
+	import Button from '$components/Button.svelte';
+
+	export let puzzles: Puzzles | undefined;
+	export let data: PageData;
+	export let form: ActionData & {
+		error?: boolean;
+		message?: string;
+	};
+
+	$: ({ puzzles } = data);
+</script>
+
 <div class="p-4">
-	<div class="w-fit">
-		<h1>Crossward man</h1>
-		<p>This is all about the crossward</p>
-	</div>
+	<section>
+		<h1 class="mb-2 mt-0 text-2xl font-medium leading-tight text-primary font-inria">Play!</h1>
+		<div
+			class="hidden bg-slate-100 px-2 pt-2 pb-8 border-solid border border-gray-200 sm:max-w-lg bg-gray"
+		>
+			<h2 class="my-2 text-md font-medium font-inria-serif">Create a puzzle</h2>
+			<form method="POST" action="?/create" use:enhance>
+				<input type="hidden" name="userEmail" value={data?.session?.user?.email} />
+				<div>
+					<label>
+						Title:
+						<input
+							type="text"
+							placeholder="A New Puzzle Title!"
+							name="title"
+							class="border-solid border border-gray-400 p-2"
+						/></label
+					>
+				</div>
+				<fieldset>
+					<legend>What size of puzzle? </legend>
+					Puzzle size?
+					<ul>
+						<li>
+							<label><input type="radio" name="size" value={MINI} checked /> Mini (5x5)</label>
+						</li>
+						<li>
+							<label><input type="radio" name="size" value={DAILY} /> Daily (15x15)</label>
+						</li>
+						<li>
+							<label><input type="radio" name="size" value={SUNDAY} /> Sunday (30x30)</label>
+						</li>
+					</ul>
+				</fieldset>
+
+				<div class="flex items-center">
+					<Button style="primary">Start Building!</Button>{#if form?.error && form?.message}
+						<p class="text-red-500 ml-4">{form.message}</p>
+					{/if}
+				</div>
+			</form>
+		</div>
+		{#if puzzles}
+			<List type="ul">
+				{#each puzzles as puzzle}
+					{#if puzzle.publishStatus === PUBLISHED}
+						<ListItem>
+							<div class="flex items-baseline font-inria-sans">
+								<a href="/puzzles/{puzzle._id}/play" class="text-sky-600">{puzzle.title}</a>
+								<div class="ml-auto">
+									<span class="text-xs text-lime-600 font-medium">PUBLISHED</span>
+									<span class="text-xs text-gray-500">2/5/2024</span>
+								</div>
+							</div>
+						</ListItem>
+					{:else if puzzle.authorEmail === data.session?.user?.email}
+						<ListItem>
+							<span>
+								{#if puzzle.publishStatus === EDIT_PUZZLE}
+									<a href="/puzzles/{puzzle._id}/editGrid" class="text-sky-400"
+										>Edit cells: {puzzle.title}</a
+									>
+								{:else if puzzle.publishStatus === EDITING_HINTS}
+									<a href="/puzzles/{puzzle._id}/editHints" class="text-sky-400"
+										>Edit hints: {puzzle.title}</a
+									>
+								{/if}</span
+							>
+						</ListItem>
+					{/if}
+				{/each}
+			</List>
+		{/if}
+	</section>
 </div>
