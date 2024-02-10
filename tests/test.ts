1849c64ba59887b652ac37d88130bae8548f6734
Adam Kiryk <adamkiryk@adams-mbp.mynetworksettings.com>
2024-02-09 19:24:52 -0500

    white space change

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,6 @@
 import { expect, test } from '@playwright/test';
 
 test('index page has expected h1', async ({ page }) => {
-	await page.goto('/');
+	await page.goto('/'); // or not
 	await expect(page.getByRole('heading', { name: 'Play!' })).toBeVisible();
 });
